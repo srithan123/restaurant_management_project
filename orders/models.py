870c9cbca0060cbff6f36c79e01e1d5c62bcd2fc
Srithan <120624626+srithan123@users.noreply.github.com>
2025-11-08 21:57:04 +0530

    55389

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,7 @@
 from django.db import models
-from django.utils import timezone
 
-class Coupon(models.Model):
-    code=models.CharField(max_length=50, unique=True)
-    discount_percentage=models.DecimalField(max_digits=5, deciaml_place=2)
-    is_active=models.BooleanField(default=True)
-    valid_from=models.DateField()
-    valid_until=models.DateField()
+class OrderStatus(models.Model):
+    name=models.CharField(max_length=50,unique=True)
 
     def __str__(self):
-        return f"{self.code} ({self.discount_percentage}% off)"
+        return self.name
