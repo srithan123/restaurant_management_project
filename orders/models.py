db92155c20ce7c87a31be5209e534956eebf3e9d
Srithan <120624626+srithan123@users.noreply.github.com>
2025-11-08 20:08:12 +0530

    55388

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from home.midels import MenuCategory
+from .order_status import OrderStatus
 # Create your models here.
+class Order(models.Model):
+    customer_name=models.CharField(max_length=100)
+    total_amount=models.DecimalField(max_digits=10, decimal_place=2)
+
+    status=models.ForeginKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
