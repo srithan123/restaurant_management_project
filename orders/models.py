639cefe35ae9dc77bed8c0f9f9ecce03680559b6
Srithan <120624626+srithan123@users.noreply.github.com>
2025-11-08 21:24:07 +0530

    55390

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from home.midels import MenuCategory
-from .order_status import OrderStatus
-# Create your models here.
-class Order(models.Model):
-    customer_name=models.CharField(max_length=100)
-    total_amount=models.DecimalField(max_digits=10, decimal_place=2)
+from django.utils import timezone
 
-    status=models.ForeginKey(OrderStatus, on_delete=models.SET_NULL, null=True)
+class Coupon(models.Model):
+    code=models.CharField(max_length=50, unique=True)
+    discount_percentage=models.DecimalField(max_digits=5, deciaml_place=2)
+    is_active=models.BooleanField(default=True)
+    valid_from=models.DateField()
+    valid_until=models.DateField()
 
     def __str__(self):
-        return f"Order #{self.id} - {self.customer_name}"
+        return f"{self.code} ({self.discount_percentage}% off)"
